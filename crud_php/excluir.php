3cbcbbffa5ed01923e535408819ee4823eb5f3d1
Matheus-O-Silva <72843539+Matheus-O-Silva@users.noreply.github.com>
2021-04-12 19:18:55 -0300

    Add files via upload

---
+++ crud_php/excluir.php
@@ -0,0 +1,36 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+use \App\Entity\Vaga;
+
+if(!isset($_GET['id']) or !is_numeric($_GET['id'])){
+    header:('location: index.php?status=error');
+    exit;
+}
+
+//CONSULTA A VAGA
+$obVaga = Vaga::getVaga($_GET['id']);
+
+//VALIDAÇÃO DA VAGA
+if(!$obVaga instanceof Vaga){
+    header('location: index.php?status=error');
+    exit;
+}
+
+//Configura horário de Brasília como horário padrão
+date_default_timezone_set('America/Sao_Paulo');
+
+//VALIDAÇÃO DO POST
+if(isset($_POST['excluir'])){
+
+    $obVaga->excluir();
+  
+    header('location: index.php?status=success');
+    exit;
+  }
+  
+  include __DIR__.'/includes/header.php';
+  include __DIR__.'/includes/confirmar_exclusao.php';
+  include __DIR__.'/includes/footer.php';
+
+?>
---
+++ crud_php/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'vagas/crud_php',
+  ),
+  'versions' => 
+  array (
+    'vagas/crud_php' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
