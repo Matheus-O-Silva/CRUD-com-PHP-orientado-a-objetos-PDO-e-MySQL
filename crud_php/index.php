52319c26fc5090a0e0fcb642b9d624bbc000057d
Matheus-O-Silva <72843539+Matheus-O-Silva@users.noreply.github.com>
2021-04-13 21:25:28 -0300

    Update index.php
    inclui barra de busca

--- crud_php/index.php
+++ crud_php/index.php
@@ -3,6 +3,21 @@ require __DIR__ . '/vendor/autoload.php';
 
 use \App\Entity\Vaga;
 
+//BUSCA
+$busca = filter_input(INPUT_GET,'busca', FILTER_SANITIZE_STRING);
+
+//CONDICOES SQL
+$condicoes = [
+    strlen($busca) ? 'titulo LIKE "%'.$busca.'%"' : null
+];
+
+//titulo LIKE "" AND ativo = "S" AND id = '' 
+
+//CLÁUSULA WHERE
+$where = implode(' AND ',$condicoes); 
+echo $where;
+exit;
+
 $vagas = Vaga::getVagas();
 
 include __DIR__ . '/includes/header.php';
